c9611e30cabc1ffb9fcc18585b9d866291431db0
Keerthana Umesh <147648970+KeerthanaUmesh@users.noreply.github.com>
2024-08-18 00:29:47 +0530

    Update swap.s

--- swap.s
+++ swap.s
@@ -1,6 +1,9 @@
+#function/sub routine for swap
+swap:
 slli x20,x20,2  #x20=k , index*4=offset
 add x22,x21,x20 #physical address calc
 lw x10,0(x22)
 lw x11,4(x22)
 sw x10,4(x22)
 sw x11,0(x22)
+jalr x0,0(x1)
